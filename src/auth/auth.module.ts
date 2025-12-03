e8ca50fb25c7abe4151f133d4d16f07b85abeb40
asadbekofficial <asaddevo211@gmail.com>
2025-12-03 17:45:06 +0500

    examples

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,18 +1,18 @@
-import { Module } from '@nestjs/common';
-import { AuthService } from './auth.service';
-import { AuthController } from './auth.controller';
-import { TypeOrmModule } from '@nestjs/typeorm';
-import { JwtModule } from '@nestjs/jwt';
-import { Auth } from 'src/entities/auth.entity';
-import { JwtStrategy } from './jwt.strategy';
+import { Module } from "@nestjs/common";
+import { AuthService } from "./auth.service";
+import { AuthController } from "./auth.controller";
+import { TypeOrmModule } from "@nestjs/typeorm";
+import { JwtModule } from "@nestjs/jwt";
+import { Auth } from "src/entities/auth.entity";
+import { JwtStrategy } from "./jwt.strategy";
 
 @Module({
   imports: [
     TypeOrmModule.forFeature([Auth]),
     JwtModule.register({
-      secret: process.env.JWT_SECRET,
-      signOptions: { expiresIn: '1h'}
-    })
+      secret: "hello",
+      signOptions: { expiresIn: "1h" },
+    }),
   ],
   controllers: [AuthController],
   providers: [AuthService, JwtStrategy],
